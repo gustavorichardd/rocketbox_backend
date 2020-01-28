d9c54bb5a8bce6300ea98527a54b9f993d33e23a
Gustavo Richard <gustavorichard1211@gmail.com>
2020-01-28 16:23:53 -0300

    Alteração do socket.io

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -14,7 +14,7 @@ class FileController {
 
         await box.save();
 
-        await req.io.sockets.in(box._id).emit('file', file);
+        req.io.sockets.in(box._id).emit('file', file);
 
         return res.json(file);
     }
